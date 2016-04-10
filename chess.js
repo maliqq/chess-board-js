b900f8b0d95879aa50e4ec04cbc732db56404c07
Malik Baktiyarov <malik@baktiyarov.com>
2016-04-11 00:12:22 +0300

    pgn parse

--- chess.js
+++ chess.js
@@ -46,12 +46,117 @@ function Piece(code) {
     name: name
   }
 }
+function parsePiece(piece) {
+  switch (piece) {
+    case 'R': return ROOK;
+    case 'N': return KNIGHT;
+    case 'B': return BISHOP;
+    case 'K': return KING;
+    case 'Q': return QUEEN;
+    default:
+      throw("unknown piece: "+ piece.toString());
+  }
+}
 
+FILES = 'abcdefgh';
+RANKS = '87654321';
 function Move(x, y) {
-  return 'abcdefgh'.charAt(y)+'87654321'.charAt(x);
+  return FILES.charAt(y)+RANKS.charAt(x);
 }
 function Move2(a) {
   return Move(a[0], a[1]);
+}
+function parseMove(data) {
+  if (data.length == 1) {
+    if (/\d/.test(data)) return {x: parseInt(data)};
+    return {y: data};
+  }
+  return {
+    x: RANKS.indexOf(data[1]),
+    y: FILES.indexOf(data[0])
+  }
+}
+
+function SAN(data) {
+  // castling
+  if (data == '1-0') return {wonByWhite: true};
+  if (data == '0-1') return {wonByBlack: true};
+  if (data == '1/2-1/2' || data == '½-½') return {draw: true};
+
+  if (data == 'O-O' || data == '0-0') {
+    return {
+      kingSideCastle: true
+    };
+  }
+  if (data == 'O-O-O' || data == '0-0-0') {
+    return {
+      queenSideCastle: true
+    };
+  }
+
+  var endWith = data[data.length-1]
+  var isMate = endWith == '#';
+  if (isMate) {
+    data = data.slice(0, -1);
+  }
+  var isCheck = endWith == '+';
+  if (isCheck) {
+    data = data.slice(0, -1);
+  }
+
+  var moveFrom = {};
+  var moveTo = {};
+
+  var piece;
+  var promotedTo;
+  var isPiece = /^[BKNQR]/.test(data[0]);
+  var isCapture = data.indexOf('x') != -1;
+
+  if (isPiece) {
+    piece = parsePiece(data[0]);
+    data = data.slice(1);
+    var m = data.match(/([a-h1-8])?x?([a-h][1-8])/);
+    if (typeof m[1] != 'undefined') {
+      moveFrom = parseMove(m[1]);
+      moveTo = parseMove(m[2]);
+    } else {
+      moveTo = parseMove(m[2]);
+    }
+  } else { // pawn
+    isPromotion = data.indexOf('=') != -1;
+    if (isPromotion) {
+      parts = data.split('=');
+      data = parts[0];
+      promotedTo = parts[1];
+    }
+    var m = data.match(/([a-h][1-8]?)?x?([a-h][1-8])/);
+    moveTo = parseMove(m[2]);
+    if (typeof m[1] != 'undefined') {
+      moveFrom = parseMove(m[1]);
+    }
+  }
+
+  return {
+    piece: piece || PAWN,
+    moveFrom: moveFrom,
+    moveTo: moveTo,
+    isCheck: isCheck, isMate: isMate, isCapture: isCapture,
+    promotedTo: promotedTo
+  }
+}
+
+function PGN(data) {
+  var moves = [];
+  data = data.slice(data.indexOf("1."))
+  movePairs = data.split(/\s?\d+\./)
+  movePairs.shift();
+  for (var i = 0; i < movePairs.length; i++) {
+    var pair = movePairs[i].split(' {')[0]; // without comment
+    var mm = pair.split(/\s+/);
+    moves.push(SAN(mm[0]));
+    moves.push(SAN(mm[1]));
+  }
+  return moves;
 }
 
 function BoardView(el) {
@@ -62,11 +167,18 @@ function BoardView(el) {
     for (var i = 0; i < board.length; i++) {
       var row = board[i];
       var r = $('<div>').addClass('row');
+      r.append($('<div>').addClass('rank').text(RANKS[i]));
       for (var j = 0; j < row.length; j++) {
         r.append(this.buildPiece(i, j, board[i][j]));
       }
       b.append(r);
     }
+    var files = $('<div>').addClass('files');
+    files.append($('<div>').addClass('corner'));
+    for (var j = 0; j < BOARD_SIZE; j++) {
+      files.append($('<div>').addClass('file').text(FILES[j]));
+    }
+    b.append(files);
     this.el.html(b);
   }
 
@@ -140,7 +252,7 @@ function BoardView(el) {
   this.suggested = function(x, y, type) {
     var el = $('#'+this.getID(x, y));
     el.addClass('suggested');
-    if (typeof type != undefined) {
+    if (typeof type != 'undefined') {
       el.addClass(type);
     }
   }
@@ -180,7 +292,6 @@ function Suggests(b) {
   this.b = b;
 
   this.filterShadows = function(x, y, isBlack, moves, isDiagonal) {
-    //return moves;
     var filtered = [];
 
     var directions = [BOARD_SIZE, BOARD_SIZE, BOARD_SIZE, BOARD_SIZE];
@@ -218,6 +329,7 @@ function Suggests(b) {
         var min = directions[__direction(_x, _y)];
         var distance = __distance(_x, _y);
         if (distance < min || min == BOARD_SIZE) {
+          console.log(Move(_x, _y), 'd=', __direction(_x, _y));
           directions[__direction(_x, _y)] = distance;
         }
       }
@@ -397,7 +509,7 @@ function Board() {
   }.bind(this);
 
   this.suggests = new Suggests(this);
-  this.whiteTurn = true;
+  this.isBlack = false;
 
   this.get = function(x, y) {
     return this.board[x][y];
@@ -439,6 +551,122 @@ function Board() {
     }
     if (p.piece == KING) return MoveType.CHECK_ON_KING;
     return MoveType.CAPTURE;
+  }
+
+  this.pgn = [];
+  this.parsePGN = function(data) {
+    this.pgn = PGN(data);
+    console.log(this.pgn);
+  }
+
+  this.forward = function() {
+    var san = this.pgn.shift();
+    if (san) {
+      d = "";
+      if (san.piece) {
+        d += Piece(san.piece).name + " ";
+      }
+      if (san.moveFrom) {
+        d += Move(san.moveFrom.x, san.moveFrom.y);
+      }
+      if (san.moveTo) {
+        d += Move(san.moveTo.x, san.moveTo.y);
+      }
+      console.log(d);
+      this.applySAN(san);
+    }
+  }
+
+  this._cacheBlack = null;
+  this._cacheWhite = null;
+  this.clearCachedPositions = function() {
+    this._cacheBlack = null;
+    this._cacheWhite = null;
+  }
+  this.cachedPositions = function(isBlack) {
+    var cached = isBlack ? this._cacheBlack : this._cacheWhite;
+    if (cached !== null) {
+      return cached;
+    }
+    var cached = {};
+    for (var i = 0; i < BOARD_SIZE; i++) {
+      for (var j = 0; j < BOARD_SIZE; j++) {
+        if (this.isEmpty(i, j)) continue;
+        var p = Piece(this.get(i, j));
+        if (p.isBlack !== isBlack) continue;
+        cached[p.piece] = cached[p.piece] || [];
+        cached[p.piece].push([i, j]);
+      }
+    }
+    if (isBlack) {
+      this._cacheBlack = cached;
+    } else {
+      this._cacheWhite = cached;
+    }
+    return cached;
+  }
+
+  this.pawnPosition = function(column, row, isBlack) {
+    var j = column;
+    if (isBlack) { // seek black pawns
+      for (var i = row-1; i >= 0; i--) {
+        if (this.isEmpty(i, j)) continue;
+        var p = Piece(this.get(i, j));
+        if (p.isBlack) return [i, j];
+      }
+    } else { // seek white pawns
+      for (var i = row+1; i < BOARD_SIZE; i++) {
+        if (this.isEmpty(i, j)) continue;
+        var p = Piece(this.get(i, j));
+        if (!p.isBlack) return [i, j];
+      }
+    }
+  }
+
+  this.piecePosition = function(piece, isBlack, options) {
+    var piecePositions = this.cachedPositions(isBlack)[piece];
+  }
+
+  this.applySAN = function(san) {
+    console.log('is black moving?', this.isBlack);
+
+    if (san.draw || san.wonByBlack || san.wonByWhite) return;
+    if (san.kingSideCastle || san.queenSideCastle) {
+      var rank = '1';
+      if (this.isBlack) {
+        rank = '8';
+      }
+      var king = parseMove('e'+rank);
+      if (board.get(king.x, king.y) != KING) {
+        throw("can't castle: king is not in correct place");
+      }
+      if (san.kingSideCastle) {
+        var kingTo = parseMove('g'+rank);
+        this.move(king.x, king.y, kingTo.x, kingTo.y);
+        var rook = parseMove('h'+rank);
+        this.move(rook.x, rook.y, king.x, king.y);
+      } else if (san.queenSideCastle) {
+        var kingTo = parseMove('c'+rank);
+        this.move(king.x, king.y, kingTo.x, kingTo.y);
+        var rook = parseMove('a'+rank);
+        var rookTo = parseMove('d'+rank);
+        this.move(rook.x, rook.y, rookTo.x, rookTo.y);
+      }
+    } else if (san.piece == PAWN) {
+      // pawn movements
+      var pos = this.pawnPosition(san.isCapture ? san.moveFrom.y : san.moveTo.y, san.moveTo.x, this.isBlack);
+      if (!pos) {
+        throw("can't move pawn");
+      }
+      this.move(pos[0], pos[1], san.moveTo.x, san.moveTo.y);
+      return
+    } else {
+      var pos = this.piecePosition(san.piece, this.isBlack, san.moveFrom);
+      this.move(pos.x, pos.y, san.moveTo.x, san.moveTo.y);
+    }
+
+    this.clearCachedPositions();
+    this.isBlack = !this.isBlack;
   }
 
   this.back = function() {
